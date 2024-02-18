90d9619dea6c68182856a7401995962b245d434d
Feng <yancaoshi@gmail.com>
2024-02-18 11:40:43 +0800

    delete useless classroom

--- src/main/java/com/lecode/eduback/mapper/ClassroomMapper.java
+++
@@ -1,8 +0,0 @@
-package com.lecode.eduback.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.lecode.eduback.model.Classroom;
-public interface ClassroomMapper extends BaseMapper<Classroom> {
-
-    void getClassroomById(Integer crid);
-}
